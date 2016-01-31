3a0f6688ee049a0d94f2de5574a7b6c5e201ec06
Tim <palapala291295@gmail.com>
2016-01-31 19:53:07 +0800

    #mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,7 +5,7 @@ class OrderMailer < ApplicationMailer
     @order_items = @order.items
     @order_info  = @order.info
 	
-	mail()
+	#mail()
 	#to: @user.email , subject: "[Artstore] 感謝您完成本次的下單，以下是您這次購物明細 #{order.token}"
   end
 end
