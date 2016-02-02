f95bd4a6d0da9c5d98b7c5b27ade2ee76f33910a
Tim <palapala291295@gmail.com>
2016-02-02 23:06:08 +0800

    items << product

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,9 +3,7 @@ class Cart < ActiveRecord::Base
 	has_many :items, through: :cart_items, source: :product
 
 	def add_product_to_cart(product)
-		ci = cart.cart_items.build
-		ci.product = product
-		ci.save
+		items << product
 	end
 
 	def total_price
