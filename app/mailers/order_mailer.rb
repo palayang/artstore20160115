2e5716b61b340a88c72069a16f12cb62b7b4bd89
Tim <palapala291295@gmail.com>
2016-02-02 19:07:35 +0800

    Test

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,9 @@
 class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order       = order
-    @user        = order.user
-    @order_items = @order.items
-    @order_info  = @order.info
-	mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    @user        = order.try(:user)
+    @order_items = @order.try(:items)
+    @order_info  = @order.try(:info)
+	mail(to: @user.try(:email), subject: 'Welcome to My Awesome Site')
   end
 end
